14f3bcb37b126384a49d5779380b8ee645d5101a
Hon <dennis.mejen@gmail.com>
2020-02-17 16:15:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// E1Lab1
+let human;
+let name = "Hon";
+human = name;
+console.log(human);
+
+// E1Lab2
+let walletMoney, parentsName, myAddress, universeAge;
+
+// E2
+let studentName = `Hon`;
+let studentAge = 29;
+let studentAddress = `Bangkok`;
+let profile = `Name: ${studentName} // Age: ${studentAge} // Address: ${studentAddress}`;
+console.log(profile)
+
+// E6.5.1
+// let myName = "Hon";
+// let answer = prompt("What's my name?");
+
+// if (myName === answer) {
+//     alert("เก่งมาก");
+// } else {
+//     alert("คุณไม่รู้จักชื่อฉัน");
+// }
+
+// E6.5.2
+// let grade = prompt('Please put in your grade.');
+
+// if (grade >= 80) {
+//     alert('A');
+// } else if (grade >= 70) {
+//     alert('B');
+// } else if (grade >= 60) {
+//     alert('C');
+// } else if (grade >= 50) {
+//     alert('D');
+// } else if (grade < 50) {
+//     alert('F');
+// }
+
+// E6.5.4
+// let age = prompt('How old are you');
+// let price;
+
+// price = (age < 18) ? "Price = " + 2000 : "Price = " + 3500;
+
+// alert(price);
